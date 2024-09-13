fe85d728cf9edcec60cf2604e9d6898bfcb04cfe
dowzwi <akkfaer3@gmail.com>
2024-09-13 19:11:16 +0600

    3month

---
+++ media/buttons.py
@@ -0,0 +1,40 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+# ===============================================================
+
+start = ReplyKeyboardMarkup(resize_keyboard=True,
+                            row_width=2)
+
+start_buttons = KeyboardButton('/start')
+mem_buttons = KeyboardButton('/mem')
+mem_all_buttons = KeyboardButton('/mem_all')
+music_buttons = KeyboardButton('/music')
+
+
+start.add(start_buttons, mem_buttons, mem_all_buttons,
+          music_buttons)
+
+# ===============================================================
+
+start_test = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    row_width=2
+    ).add(
+KeyboardButton('/start')
+)
+
+# ===============================================================
+
+start_test_1 = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    row_width=2
+    )
+
+start_test_1.add(
+    KeyboardButton('/start'),
+    KeyboardButton('/mem'),
+    KeyboardButton('/mem_all'),
+    KeyboardButton('/music')
+)
+
+# ===============================================================
---
+++ media/main.py
@@ -0,0 +1,25 @@
+import logging
+from aiogram.utils import executor
+from buttons import start_test
+from config import bot, dp, admin
+from Handlers import commands, echo, quiz, FSM_registration
+
+
+async def on_startup(_):
+    for i in admin:
+        await bot.send_message(chat_id=i, text="Бот включен!",
+                               reply_markup=start_test)
+
+
+commands.register_commands(dp)
+quiz.register_quiz(dp)
+FSM_registration.register_fsm_reg(dp)
+
+
+echo.register_echo(dp)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+ 
